53bef7c9d766ab784c96d9b04aa5bc0c95f1d635
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-11-18 17:56:31 +0100

    Make image view editor

--- PhotoEdit/Views/ImageEditorView.swift
+++ PhotoEdit/Views/ImageEditorView.swift
@@ -8,13 +8,114 @@
 import SwiftUI
 
 struct ImageEditorView: View {
+    @State var image: UIImage = UIImage(named: "image-1")!
+    
+    private var columns = [
+        GridItem(.fixed(100), spacing: 2.0)
+    ]
+    
     var body: some View {
-       Text("Text")
+        VStack {
+            Spacer()
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .padding([.top], 40)
+                .frame(maxWidth: .infinity)
+            Spacer()
+            LazyHGrid(
+                rows: columns,
+                alignment: .top,
+                spacing: 2.0,
+                pinnedViews: []
+            ) {
+                ForEach(0...100, id: \.self) { id in
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.red)
+                }
+            }
+            .frame(height: 100.0)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    
+                } label: {
+                    Text("Cancel")
+                }
+            }
+            ToolbarItem(placement: .status) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .status) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .status) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                }
+            }
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    
+                } label: {
+                    Text("Done")
+                }
+            }
+        }
+        .navigationTitle(
+            Text("Filters")
+        )
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color.black)
     }
 }
 
+
 struct ImageEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        ImageEditorView()
+        NavigationStack {
+            ImageEditorView()
+        }
     }
 }
