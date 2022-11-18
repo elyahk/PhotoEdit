4630f366fc5e560d5c5ea9274fb123d91784400e
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-11-18 16:35:16 +0100

    Create ImageEditorView

---
+++ PhotoEdit/Views/ImageEditorView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageEditorView.swift
+//  PhotoEdit
+//
+//  Created by Eldorbek Nusratov on 18/11/22.
+//
+
+import SwiftUI
+
+struct ImageEditorView: View {
+    var body: some View {
+       Text("Text")
+    }
+}
+
+struct ImageEditorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageEditorView()
+    }
+}
