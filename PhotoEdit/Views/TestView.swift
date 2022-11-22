4edc7acafa154dea786884a83637e6c95b82deb1
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-11-22 15:30:33 +0100

    Create TestView for experiment purpose

---
+++ PhotoEdit/Views/TestView.swift
@@ -0,0 +1,26 @@
+//
+//  TestView.swift
+//  PhotoEdit
+//
+//  Created by Eldorbek Nusratov on 21/11/22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    @State var state = 0
+    
+    var body: some View {
+        VStack {
+            Text("State: \(state)")
+        }
+        
+        
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
